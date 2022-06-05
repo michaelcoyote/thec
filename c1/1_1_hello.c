9fc57ec88f6dfe627a0edfb07e8fee3b62683984
michael guldan <michaelguldan@gmail.com>
2022-06-05 12:08:43 -0700

    rename hello.c with book information and add comment

---
+++ c1/1_1_hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*the first C program
+ *
+ * Introduces the standard library stdio.h,
+ * functions and specifically the main() function
+ * also the concept of calling functions,
+ * specifically the printf() function.
+ * */
+
+int main()
+{
+	printf("hello nasty\n");
+}
+
